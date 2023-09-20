076e010d981103807bf0ff3d9f68101577744706
hkve <haakon.kvernmoen@gmail.com>
2023-09-20 14:19:36 +0200

    Added very simple DIIS test

--- tests/test_mixer.py
+++ tests/test_mixer.py
@@ -1,5 +1,5 @@
 from mypack.basis import PyscfBasis
-from mypack.mix import AlphaMixer
+from mypack.mix import AlphaMixer, DIISMixer
 from mypack.hf import HF
 from unittest import TestCase
 
@@ -18,6 +18,19 @@ class TestMixer(TestCase):
 
         self.assertAlmostEqual(E_nomix, hf.energy())
         self.assertTrue(iter_nomix < hf._iters)
+
+    def test_DIIS_with_hf(self):
+        b = PyscfBasis(atom="Ne 0 0 0", basis="cc-pVDZ")
+        hf = HF(b)
+        hf.run(tol=1e-8)
+        E_nomix, iter_nomix = hf.energy(), hf._iters
+
+        hf = HF(b)
+        hf.mixer = DIISMixer(n_vectors=4)
+        hf.run(tol=1e-8)
+
+        self.assertAlmostEqual(E_nomix, hf.energy())
+        self.assertTrue(iter_nomix > hf._iters)
 
     def test_alpha_mixer(self):
         mixer = AlphaMixer(alpha=0.1)
