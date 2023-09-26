ba89dca1e8afbd9ae1bd56a814d8ad211eb4ff7d
hkve <haakon.kvernmoen@gmail.com>
2023-09-26 18:24:54 +0200

    Fixed test after rename

--- tests/test_mixer.py
+++ tests/test_mixer.py
@@ -11,28 +11,28 @@ class TestMixer(TestCase):
         hf.mixer = RelaxedMixer(alpha=0)
         hf.run(tol=1e-8)
 
-        E_nomix, iter_nomix = hf.energy(), hf._iters
+        E_nomix, iter_nomix = hf.energy(), hf.iters
 
         hf = HF(b)
         hf.mixer = RelaxedMixer(alpha=0.5)
         hf.run(tol=1e-8)
 
         self.assertAlmostEqual(E_nomix, hf.energy())
-        self.assertTrue(iter_nomix < hf._iters)
+        self.assertTrue(iter_nomix < hf.iters)
 
     def test_DIIS_with_hf(self):
         b = PyscfBasis(atom="Ne 0 0 0", basis="cc-pVDZ")
         hf = HF(b)
         hf.mixer = RelaxedMixer(alpha=0)
         hf.run(tol=1e-8)
-        E_nomix, iter_nomix = hf.energy(), hf._iters
+        E_nomix, iter_nomix = hf.energy(), hf.iters
 
         hf = HF(b)
         hf.mixer = DIISMixer(n_vectors=4)
         hf.run(tol=1e-8)
 
         self.assertAlmostEqual(E_nomix, hf.energy())
-        self.assertTrue(iter_nomix > hf._iters)
+        self.assertTrue(iter_nomix > hf.iters)
 
     def test_alpha_mixer(self):
         mixer = RelaxedMixer(alpha=0.1)
