0a87d610ae8eb60cc715d060c11519abd57aec29
hkve <haakon.kvernmoen@gmail.com>
2023-10-02 17:04:19 +0200

    Mixed in with ccsd as well

--- src/clusterfock/cc/ccsd.py
+++ src/clusterfock/cc/ccsd.py
@@ -1,6 +1,7 @@
 import numpy as np
 from clusterfock.basis import Basis
 from clusterfock.cc.coupledcluster import CoupledCluster
+from clusterfock.cc.parameter import CoupledClusterParameter
 
 from clusterfock.cc.rhs.t_CCSD import amplitudes_ccsd
 from clusterfock.cc.rhs.t_inter_CCSD import amplitudes_intermediates_ccsd
@@ -10,30 +11,34 @@ class GCCSD(CoupledCluster):
     def __init__(self, basis: Basis, intermediates: bool = True):
         assert not basis.restricted, "CCD can not deal with restricted basis"
 
-        t_amplitude_orders = ["S", "D"]
-        super().__init__(basis, t_amplitude_orders)
-        np.fill_diagonal(self._f, 0)
-        # self._f.fill(0)
+        orders = [1, 2]
+        super().__init__(basis, orders)
 
         self.rhs = amplitudes_intermediates_ccsd if intermediates else amplitudes_ccsd
 
-    def _next_t_iteration(self, amplitudes: dict) -> dict:
+    def _next_t_iteration(self, t: CoupledClusterParameter) -> dict:
         basis = self.basis
 
-        t1_next, t2_next = self.rhs(
-            t1=amplitudes["S"],
-            t2=amplitudes["D"],
+        rhs1, rhs2 = self.rhs(
+            t1=t[1],
+            t2=t[2],
             u=basis.u,
             f=self._f,
             v=basis.v,
             o=basis.o,
         )
 
-        return {"S": t1_next * self._epsinv["S"], "D": t2_next * self._epsinv["D"]}
+        rhs = CoupledClusterParameter(t.orders, t.N, t.M)
+        rhs.initialize_dicts({
+            1: rhs1,
+            2: rhs2
+        })
 
-    def _evaluate_cc_energy(self, t_amplitudes: np.ndarray) -> float:
+        return rhs
+
+    def _evaluate_cc_energy(self, t: CoupledClusterParameter) -> float:
         u, o, v = self.basis.u, self.basis.o, self.basis.v
-        t1, t2 = t_amplitudes["S"], t_amplitudes["D"]
+        t1, t2 = t[1], t[2]
 
         e = 0
         e += np.einsum("ia,ai->", self._f[o, v], t1, optimize=True)
