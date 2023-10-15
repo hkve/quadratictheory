65743ea873cdec26a931c0ae1c71123592101206
hkve <haakon.kvernmoen@gmail.com>
2023-10-15 18:02:16 +0200

    Test for internal consistency between amplitudes calculated with and without intermediates, where the density matrix difference is checked

---
+++ tests/test_coupledcluster_densities.py
@@ -0,0 +1,40 @@
+import numpy as np
+from unittest import TestCase
+from clusterfock.hf import RHF
+from clusterfock.cc import GCCD
+from clusterfock.basis import PyscfBasis
+
+class TestCoupledClusterDensities(TestCase):
+    def compare_raw_vs_intermediate(self, atom, basis, tol=1e-8):
+        rbasis = PyscfBasis(atom=atom, basis=basis, restricted=True)
+
+        hf = RHF(rbasis)
+        hf.run(tol=tol, vocal=False)
+        rbasis.change_basis(hf.C, inplace=True)
+        gbasis = rbasis.from_restricted(inplace=False)
+
+        raw_ccd = GCCD(gbasis, intermediates=False).run(tol=tol, include_l=True)
+        inter_ccd = GCCD(gbasis, intermediates=True).run(tol=tol, include_l=True)
+
+        raw_ccd.calculate_density()
+        inter_ccd.calculate_density()
+
+        raw_rho = raw_ccd.rho_ob
+        inter_rho = inter_ccd.rho_ob
+
+        N = gbasis.N
+        self.assertAlmostEqual(np.trace(raw_rho), N, places=6)
+        self.assertAlmostEqual(np.trace(inter_rho), N, places=6)
+
+        diff = np.abs(raw_rho - inter_rho).ravel()
+        all_close = np.all(diff < 1e-6)
+        self.assertTrue(all_close, msg=f"Difference between intermediate and raw results for densities max(diff) {diff.max()} > 1e-6")
+
+    def test_ccd_He(self):
+        self.compare_raw_vs_intermediate(atom="He 0 0 0", basis="cc-pVDZ")
+
+    def test_ccd_Be(self):
+        self.compare_raw_vs_intermediate(atom="Be 0 0 0", basis="cc-pVDZ")
+
+    def test_ccd_LiH(self):
+        self.compare_raw_vs_intermediate(atom="Li 0 0 0; H 0 0 1.2", basis="cc-pVDZ")
