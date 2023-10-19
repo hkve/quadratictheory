1f2b9692cb2b32bd374c5bb2781d6cfa4d587c42
hkve <haakon.kvernmoen@gmail.com>
2023-10-19 12:05:08 +0200

    Added some expvals

--- analysis/benchmarks/cc_densities.py
+++ analysis/benchmarks/cc_densities.py
@@ -10,41 +10,66 @@ from cccbdb_comparison import angstrom_to_bohr
 import numpy as np
 
 
-def ccd(atom, basis, tol=1e-4):
+def run_cf(atom, basis):
     b = PyscfBasis(atom=atom, basis=basis, restricted=False)
 
     hf = HF(b).run()
     b.change_basis(hf.C)
     ccd = CCD(b, intermediates=False).run(include_l=True, tol=1e-8)
     ccd.one_body_density()
-    rho_cf = ccd.rho_ob
 
+    return ccd
+
+def run_hyqd(atom, basis):
     system = construct_pyscf_system_rhf(molecule=angstrom_to_bohr(atom), basis=basis)
     ccd_hyqd = HyCCD(system)
     ccd_hyqd.compute_ground_state(t_kwargs={"tol": 1e-8}, l_kwargs={"tol": 1e-8})
+    
+    return ccd_hyqd.compute_one_body_density_matrix().real
 
-    rho_hyqd = ccd_hyqd.compute_one_body_density_matrix().real
+def ccd(atom, basis, tol=1e-4):
+    ccd = run_cf(atom, basis)
+    rho_cf = ccd.rho_ob
+
+    rho_hyqd = run_hyqd(atom, basis)
 
     diff = np.abs(rho_cf - rho_hyqd)
 
     indicies = np.argwhere(diff > tol)
 
+    print(f"For {atom}")
+    print(f"Missmatched {len(indicies)}/{rho_cf.size}")
     for index in indicies:
         i, j = index
-        # if i >= j:
-        print(f"CF: {rho_cf[i,j]:.4e}, HYQD: {rho_hyqd[i,j]:.4e}, (i,j) = ({i},{j})")
+        if i >= j:
+            print(f"CF: {rho_cf[i,j]:.4e}, HYQD: {rho_hyqd[i,j]:.4e}, (i,j) = ({i},{j})")
 
-    print(f"Missmatched {len(indicies)}/{rho_cf.size}")
+def ccd_expvals(atom, basis):
+    b = PyscfBasis(atom, basis, restricted=False)
+    hf = HF(b).run()
+    b.change_basis(hf.C)
 
+    rho_cf = run_cf(atom, basis).rho_ob
+    rho_hyqd = run_hyqd(atom, basis)
+
+    r_cf = np.einsum("pq,iqp->i", rho_cf, b.r)
+    r_hyqd = np.einsum("pq,iqp->i", rho_hyqd, b.r)
+
+    print(f"For {atom}")
+    print(f"{r_cf = }\n{r_hyqd = }")
 
 def ccsd(atom, basis):
     pass
 
 
 def main():
-    ccd("Li 0 0 0; H 0 0 1.2", "cc-pVDZ")
     # ccd("He 0 0 0", basis="cc-pVDZ")
     # ccd("Be 0 0 0", basis="cc-pVDZ")
+    # ccd("Li 0 0 0; H 0 0 1.2", "cc-pVDZ")
+
+    ccd_expvals("He 0 0 0", basis="cc-pVDZ")
+    ccd_expvals("Be 0 0 0", basis="cc-pVDZ")
+    ccd_expvals("Li 0 0 0; H 0 0 1.2", "cc-pVDZ")
 
 
 if __name__ == "__main__":
