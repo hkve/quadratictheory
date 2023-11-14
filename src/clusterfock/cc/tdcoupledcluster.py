0866ccf0afa01cef10b2ff3fda4dc25098e65e64
hkve <haakon.kvernmoen@gmail.com>
2023-11-14 10:32:31 +0100

    Added while loop for t integration instead of t

--- src/clusterfock/cc/tdcoupledcluster.py
+++ src/clusterfock/cc/tdcoupledcluster.py
@@ -9,12 +9,12 @@ from scipy.integrate import complex_ode
 
 class TimeDependentCoupledCluster:
     def __init__(
-        self, cc: CoupledCluster, time: tuple = (0, 10.0, 0.0001), integrator="Rk4Integrator"
+        self, cc: CoupledCluster, time: tuple = (0, 1.0, 0.0001), integrator="Rk4Integrator"
     ):
         self.cc = cc
         self.basis = cc.basis
 
-        self._t_start, self._t_end, self.dt = time
+        self._t_start, self._t_end, self._dt = time
         self._integrator = integrator
 
     def run(self, vocal=False):
@@ -28,22 +28,27 @@ class TimeDependentCoupledCluster:
             cc._l.dtype = complex
 
         y_initial, self.t_slice, self.l_slice = merge_to_flat(cc._l, cc._l)
-        times = self.t
+        t_start, t_end, dt = self.t_start, self.t_end, self.dt
 
         integrator = complex_ode(self.f)
         integrator.set_integrator(self._integrator, dt=self.dt)
-        integrator.set_initial_value(y_initial, times[0])
+        integrator.set_initial_value(y_initial)
 
-        n = 100000
-        for i in range(1,n):
-            integrator.integrate(times[i])
+        print(f"Running {int((t_end - t_start)/dt)+1} points")
+        assert t_end > t_start
+        
+        t = t_start
+        while t < t_end:
+            t += dt
+            integrator.integrate(t)
+
 
     def f(self, t, y):
         # y comes in flat and should return flat, but be evaluated in the meen while
         # it contains both t and l
         basis, cc = self.basis, self.cc
         cc._t.from_flat(y[self.t_slice])        
-        cc._l.from_flat(y[self.l_slice])        
+        cc._l.from_flat(y[self.l_slice])
         
         # Here I should add rhs and lhs updates to
         t_dot = 1j*cc._next_t_iteration(cc._t)
@@ -59,5 +64,20 @@ class TimeDependentCoupledCluster:
 
     @dt.setter
     def dt(self, dt):
-        self.t = np.arange(0, self._t_end + dt, step=dt)
         self._dt = dt
+
+    @property
+    def t_start(self):
+        return self._t_start
+
+    @dt.setter
+    def t_start(self, t_start):
+        self._t_start = t_start
+
+    @property
+    def t_end(self):
+        return self._t_end
+
+    @t_end.setter
+    def t_end(self, t_end):
+        self._t_end = t_end
