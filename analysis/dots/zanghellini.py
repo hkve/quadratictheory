8423079300171c86a49cd9efd0ebbb42b8e6db87
hkve <haakon.kvernmoen@gmail.com>
2023-11-14 18:53:45 +0100

    Zanghellini CCD densities

--- analysis/dots/zanghellini.py
+++ analysis/dots/zanghellini.py
@@ -10,16 +10,18 @@ def plot_density():
     rho_hf = basis.density(hf.rho)
     basis.change_basis(hf.C)
 
-    cc = cf.CCSD(basis=basis).run(include_l=True)
-    rho_cc = cc.one_body_density()
-    rho_cc = np.einsum("ap,pq,bq->ab", hf.C, rho_cc, hf.C)
-    rho_cc = basis.density(rho_cc)
-
-    x = basis.x
-
     fig, ax = plt.subplots()
+    x = basis.x
     ax.plot(x, rho_hf, label="HF")
-    ax.plot(x, rho_cc, label="CCSD")
+    
+    methods = [cf.CCD, cf.CCSD]
+
+    for CC in methods: 
+        cc = CC(basis=basis).run(include_l=True, tol=1e-6, vocal=True)
+        rho_cc = cc.one_body_density()
+        rho_cc = np.einsum("ap,pq,bq->ab", hf.C, rho_cc, hf.C)
+        rho_cc = basis.density(rho_cc)
+        ax.plot(x, rho_cc, label=CC.__name__)
 
     ax.legend()
     ax.set(xlim=(-6,6), ylim=(0,0.4))
