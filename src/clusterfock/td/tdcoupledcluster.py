6374254a8fa384d563dfaccbc7fd8dff2e4c26d1
hkve <haakon.kvernmoen@gmail.com>
2023-11-15 17:33:03 +0100

    I think it works now

--- src/clusterfock/td/tdcoupledcluster.py
+++ src/clusterfock/td/tdcoupledcluster.py
@@ -4,8 +4,8 @@ from clusterfock.cc.parameter import CoupledClusterParameter, merge_to_flat
 from clusterfock.cc.coupledcluster import CoupledCluster
 import numpy as np
 
-from scipy.integrate import complex_ode
-
+from scipy.integrate import complex_ode, ode
+from rk4_integrator.rk4 import Rk4Integrator
 
 class TimeDependentCoupledCluster:
     def __init__(
@@ -25,9 +25,12 @@ class TimeDependentCoupledCluster:
     def run(self, vocal=False):
         cc, basis = self.cc, self.basis
 
-        if not (cc.t_info["run"] and cc.l_info["run"]):
-            cc._f += self.external_one_body(self.t_start, basis)
+        if not (cc.t_info["run"] or cc.l_info["run"]):
+            if self._has_td_ob:
+                external_contribution = self.external_one_body(self._t_start, basis)
+                cc._f += external_contribution
             cc.run(include_l=True, vocal=vocal)
+            print(f"Energy: {cc.energy()}")
         if not basis.dtype == complex:
             basis.dtype = complex
             cc._t.dtype = complex
@@ -38,50 +41,57 @@ class TimeDependentCoupledCluster:
         self._l0 = cc._l.copy()
 
         y_initial, self.t_slice, self.l_slice = merge_to_flat(cc._l, cc._l)
-        t_start, t_end, dt = self.t_start, self.t_end, self.dt
+        t_start, t_end, dt = self._t_start, self._t_end, self._dt
+
+        assert dt > 0
+        assert t_end > t_start
+        n_time_points = int(np.ceil((t_end - t_start)/dt)) + 1
 
         integrator = complex_ode(self.rhs)
-        integrator.set_integrator(self._integrator, dt=self.dt)
+        integrator.set_integrator(self._integrator, dt=dt)
         integrator.set_initial_value(y_initial, t_start)
-
-        n_time_points = int(np.ceil((t_end - t_start)/dt))+1
-        assert t_end > t_start
 
         energy = np.zeros(n_time_points, dtype=basis.dtype)
         overlap = np.zeros(n_time_points, dtype=basis.dtype)
+        
         energy[0] = cc.energy()
         overlap[0] = cc.overlap(self._t0, self._l0, cc._t, cc._l)
 
-        t = t_start
-        counter = 0
-        while t < t_end:
-            t += dt
+        t, counter = dt, 0
+        while integrator.successful() and t < t_end:
+            integrator.integrate(t) # Integrates to y(t)
+            
+            cc._t.from_flat(integrator.y[self.t_slice])
+            cc._l.from_flat(integrator.y[self.l_slice])
+            
             counter += 1
-            integrator.integrate(t)
+            print(f"Done {counter}/{n_time_points}, t = {t}")
+            if counter >= n_time_points:
+                break
             energy[counter] = cc.energy()
-            overlap_complex = cc.overlap(self._t0, self._l0, cc._t, cc._l)
-            overlap[counter] = overlap_complex.real**2 + overlap_complex.imag**2
-        
+            overlap[counter] = cc.overlap(self._t0, self._l0, cc._t, cc._l)
+            t += dt
+
         return np.arange(t_start, t_end+dt, dt), energy, overlap
     
 
     def rhs(self, t, y):
-        # y comes in flat and should return flat, but be evaluated in the meen while
-        # it contains both t and l
         basis, cc = self.basis, self.cc
-        cc._t.from_flat(y[self.t_slice])        
+
+        # Update t and l amplitudes in basis
+        cc._t.from_flat(y[self.t_slice])       
         cc._l.from_flat(y[self.l_slice])
-        
+
         if self._has_td_ob:
-            cc._f = basis.f + self.external_one_body(t, basis)
+            external_contribution = self.external_one_body(t, basis)
+            cc._f = basis.f + external_contribution
 
-        # Here I should add rhs and lhs updates to
-        t_dot = 1j*cc._next_t_iteration(cc._t)
-        l_dot = -1j*cc._next_l_iteration(cc._t, cc._l)
+        t_dot = -1j*cc._next_t_iteration(cc._t)
+        l_dot = 1j*cc._next_l_iteration(cc._t, cc._l)
 
-        y, _, _ = merge_to_flat(t_dot, l_dot)
+        y_dot, _, _ = merge_to_flat(t_dot, l_dot)
 
-        return y
+        return y_dot
 
     @property
     def external_one_body(self):
@@ -91,27 +101,3 @@ class TimeDependentCoupledCluster:
     def external_one_body(self, func_ob):
         self._has_td_ob = True
         self._td_ob = func_ob
-
-    @property
-    def dt(self):
-        return self._dt
-
-    @dt.setter
-    def dt(self, dt):
-        self._dt = dt
-
-    @property
-    def t_start(self):
-        return self._t_start
-
-    @dt.setter
-    def t_start(self, t_start):
-        self._t_start = t_start
-
-    @property
-    def t_end(self):
-        return self._t_end
-
-    @t_end.setter
-    def t_end(self, t_end):
-        self._t_end = t_end
