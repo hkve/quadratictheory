13d074090611576de787463d9a26823b2346c299
hkve <haakon.kvernmoen@gmail.com>
2023-11-21 17:51:27 +0100

    A factor of two

--- tests/test_hartreefock.py
+++ tests/test_hartreefock.py
@@ -45,15 +45,46 @@ class TestHartreeFock(TestCase):
         self.assertAlmostEqual(E_rhf, E_pyscf)
         self.assertAlmostEqual(E_hf, E_pyscf)
 
+    def zero_posistion(self, atom, basis, restricted):
+        basis = PyscfBasis(atom=atom, basis=basis, restricted=restricted)
+
+        hf = HF(basis).run()
+
+        r = hf.one_body_expval(basis.r)
+
+        for i in range(3):
+            self.assertAlmostEqual(r[i], 0)
+
+    def translate_posistion(self, atom, atom_translated, translation, basis, restricted):
+        first = PyscfBasis(atom=atom, basis=basis, restricted=restricted)
+        hf = HF(first).run()
+        r = hf.one_body_expval(first.r)
+
+        second = PyscfBasis(atom=atom_translated, basis=basis, restricted=restricted)
+        hf = HF(second).run()
+        r_translated = hf.one_body_expval(second.r)
+
+        translation_bohr = [1.8897259886*t for t in translation]
+        for i in range(3):
+            self.assertAlmostEqual(r_translated[i], r[i]+translation_bohr[i])
+
     # Atoms
     def test_He(self):
         self.compare_with_pyscf(atom="He 0 0 0", basis="cc-pVDZ")
+        self.zero_posistion(atom="He 0 0 0", basis="cc-pVDZ", restricted=True)
+        self.zero_posistion(atom="He 0 0 0", basis="cc-pVDZ", restricted=False)
+        self.translate_posistion(atom="He 0 0 0", atom_translated="He 0 0 1", translation=[0,0,1], basis="cc-pVDZ", restricted=False)
 
     def test_Be(self):
         self.compare_with_pyscf(atom="Be 0 0 0", basis="cc-pVDZ")
+        self.zero_posistion(atom="Be 0 0 0", basis="cc-pVDZ", restricted=True)
+        self.zero_posistion(atom="Be 0 0 0", basis="cc-pVDZ", restricted=False)
+
 
     def test_Ne(self):
         self.compare_with_pyscf(atom="Ne 0 0 0", basis="cc-pVDZ")
+        self.zero_posistion(atom="Ne 0 0 0", basis="cc-pVDZ", restricted=True)
+        self.zero_posistion(atom="Ne 0 0 0", basis="cc-pVDZ", restricted=False)
 
     # Molecules
     def test_H2(self):
