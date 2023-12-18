3560007bd61d9c2da95e6e6dc997be26725079f5
hkve <haakon.kvernmoen@gmail.com>
2023-12-18 15:13:52 +0100

    Added my own diss code

--- analysis/chemistry/dissociation.py
+++ analysis/chemistry/dissociation.py
@@ -1,98 +1,14 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import clusterfock as cf
+import plot_utils as pu
 
-import pyscf
-from pyscf.cc.ccd import CCD as pyscfCCD
+x = np.linspace(-6,6,1000)
 
+fig, ax = plt.subplots()
 
-def get_dissociation(df):
-    for col in ["CCD", "QCCD", "VCCD"]:
-        df[col] = df[col] + df.FCI
+for i in range(1,5):
+    ax.plot(x, np.cos(i*x))
 
-    E0 = df.FCI.iloc[-1]
-    df_diss = df.copy()
-
-    for col in ["CCD", "QCCD", "VCCD", "FCI"]:
-        df_diss[col] = df_diss[col] - E0
-
-    return df_diss, E0
-
-
-def run_CCD_pyscf(atom, basis, tol=1e-8):
-    basis = cf.PyscfBasis(atom, basis, restricted=True)
-    hf_pyscf = pyscf.scf.HF(basis.mol).run(verbose=0, tol=tol)
-    ccd_pyscf = pyscfCCD(hf_pyscf).run(verbose=0, tol=tol)
-
-    print(f"{atom} hf? {hf_pyscf.converged} ccd? {ccd_pyscf.converged}")
-    return ccd_pyscf.e_tot
-
-
-def run_CCD(atom, basis, tol=1e-8):
-    basis = cf.PyscfBasis(atom, basis, restricted=True)
-    hf = cf.HF(basis).run()
-
-    basis.change_basis(hf.C)
-    basis.from_restricted()
-
-    ccd = cf.CCD(basis).run(vocal=False, tol=tol, maxiters=100)
-
-    energy = 0
-    if not ccd.converged:
-        energy = np.nan
-    else:
-        energy = ccd.energy()
-
-    print(f"{atom}, hf? = {hf.converged} after {hf.iters}, ccd? {ccd.converged} after {ccd.iters}")
-    return energy
-
-
-def plot_N2():
-    df = pd.read_csv("vanvoorhis_headgordon/n2.txt", sep=" ", skiprows=5, header=0, index_col=False)
-
-    df_diss, E0 = get_dissociation(df)
-
-    fig, ax = plt.subplots()
-    for col in ["CCD", "FCI"]:
-        ax.scatter(df_diss.R, df_diss[col], label=col)
-
-    cf_CCD = np.zeros_like(df.R.to_numpy()) * np.nan
-    pyscf_CCD = np.zeros_like(df.R.to_numpy()) * np.nan
-    for i, r in enumerate(df_diss.R[:-2]):
-        cf_CCD[i] = run_CCD(atom=f"N 0 0 0; N 0 0 {r}", basis="sto-3g") - E0
-        pyscf_CCD[i] = run_CCD_pyscf(atom=f"N 0 0 0; N 0 0 {r}", basis="sto-3g") - E0
-
-    ax.scatter(df_diss.R, cf_CCD, label="cf.CCD")
-    ax.scatter(df_diss.R, pyscf_CCD, label="pyscf.CCD")
-    ax.legend()
-    ax.set(xlabel=r"$\Delta R$", ylabel=r"$E(N_2) - 2E(N)$")
-    plt.show()
-
-
-def plot_HF():
-    df = pd.read_csv("vanvoorhis_headgordon/hf.txt", sep=" ", skiprows=5, header=0, index_col=False)
-
-    df_diss = df
-    df_diss, E0 = get_dissociation(df)
-
-    fig, ax = plt.subplots()
-    for col in ["CCD", "FCI"]:
-        ax.scatter(df_diss.R, df_diss[col], label=col)
-
-    cf_CCD = np.zeros_like(df.R.to_numpy()) * np.nan
-    pyscf_CCD = np.zeros_like(df.R.to_numpy()) * np.nan
-    for i, r in enumerate(df_diss.R):
-        cf_CCD[i] = run_CCD(atom=f"F 0 0 0; H 0 0 {r}", basis="DZ") - E0
-        pyscf_CCD[i] = run_CCD_pyscf(atom=f"F 0 0 0; H 0 0 {r}", basis="DZ") - E0
-
-    ax.scatter(df_diss.R, cf_CCD, label="cf.CCD")
-    ax.scatter(df_diss.R, pyscf_CCD, label="pyscf.CCD")
-    ax.legend()
-    ax.set(xlabel=r"$\Delta R$", ylabel=r"$E(HF) - E(H) - E(F)$")
-    plt.show()
-
-
-if __name__ == "__main__":
-    plot_N2()
-    # plot_HF()
+pu.save("test_fig")
+plt.show()
---
+++ analysis/chemistry/plot_utils.py
@@ -0,0 +1,61 @@
+import pathlib as pl
+import matplotlib
+import matplotlib.pyplot as plt
+from cycler import cycler
+
+def make_figs_path(filename):
+    cur_path = pl.Path(__file__)
+    root_path = cur_path
+
+    while root_path.name != "analysis":
+        root_path = root_path.parent
+
+    figs_path = root_path / pl.Path("plots")
+
+    if not figs_path.exists():
+        return None
+    if not filename.endswith(".pdf"):
+        filename += ".pdf"
+
+    figs_path /= filename
+
+    return str(figs_path)
+
+def save(filename):
+    if filename:
+        filename = make_figs_path(filename)
+        plt.savefig(filename)
+
+
+colors = ["black", "orange", "green", "red", "purple", "blue"]
+markers = ["o", "^", "s", "x", "+", "<"]
+
+# Set all fonts to be equal to tex
+# https://stackoverflow.com/questions/11367736/matplotlib-consistent-font-using-latex
+plt.rcParams["mathtext.fontset"] = "stix"
+plt.rcParams["font.family"] = "STIXGeneral"
+plt.rcParams["text.usetex"] = True
+
+# Saving parameters
+plt.rcParams["savefig.dpi"] = 300
+
+# Figure options, set tight layout
+plt.rc("figure", autolayout=True)
+
+# Font sizes
+plt.rc("axes", titlesize=18, labelsize=16, prop_cycle=cycler('color', colors))
+plt.rc("legend", fontsize=14, shadow=True)
+
+# Tick parameters
+_ticks_default_parameters = {
+    "labelsize": 12
+}
+plt.rc("xtick", **_ticks_default_parameters)
+plt.rc("ytick", **_ticks_default_parameters)
+
+# Line options
+plt.rc("lines", linewidth=2)
+
+
+# To see more paramteres, print the possible options:
+# print(plt.rcParams)
