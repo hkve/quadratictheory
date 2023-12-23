d464f44026b459b25196e74fa1c48ff7ce819258
hkve <haakon.kvernmoen@gmail.com>
2023-12-23 14:45:46 +0100

    Fixed bug

--- symbolic/qccsd.py
+++ symbolic/qccsd.py
@@ -17,17 +17,17 @@ def run(dr):
 
     # Performing algebraic derivatives
     energy_eq = ((1 + L + L*L/2)*ham_bar).eval_fermi_vev().simplify()
-    drutils.timer.tock("QCCD lagrangian addition done")
+    drutils.timer.tock("QCCSD lagrangian addition done")
 
     t1_terms = energy_eq.diff(t1[a,i]).simplify()
-    drutils.timer.tock("QCCD t1 done")
+    drutils.timer.tock("QCCSD t1 done")
     t2_terms = (4*drutils.diff_rk2_antisym(energy_eq, l2, (i,j), (a,b))).simplify()
-    drutils.timer.tock("QCCD t2 done")
+    drutils.timer.tock("QCCSD t2 done")
 
     l1_terms = energy_eq.diff(l1[a,i]).simplify()
-    drutils.timer.tock("QCCD l1 done")
+    drutils.timer.tock("QCCSD l1 done")
     l2_terms = (4*drutils.diff_rk2_antisym(energy_eq, t2, (i,j), (a,b))).simplify()
-    drutils.timer.tock("QCCD l2 done")
+    drutils.timer.tock("QCCSD l2 done")
     drutils.timer.tock("DONE DOING BASE ALGEBRA")
 
     # Define equations and saving einsums, save algebraic equations in raw form
@@ -37,11 +37,11 @@ def run(dr):
     t1_equations = drutils.define_rk1_rhs(dr, t1_terms)
     l1_equations = drutils.define_rk1_rhs(dr, l1_terms)
     grutils.einsum_raw(dr, "qccsd_t1_from_lag", [t1_equations])
-    grutils.einsum_raw(dr, "qccsd_t2_from_lag", [t2_equations])
+    grutils.einsum_raw(dr, "qccsd_l1_from_lag", [l1_equations])
 
     t2_equations = drutils.define_rk2_rhs(dr, t2_terms)
     l2_equations = drutils.define_rk2_rhs(dr, l2_terms)
-    grutils.einsum_raw(dr, "qccsd_l1_from_lag", [l1_equations])
+    grutils.einsum_raw(dr, "qccsd_t2_from_lag", [t2_equations])
     grutils.einsum_raw(dr, "qccsd_l2_from_lag", [l2_equations])
     
     drutils.save_html(dr, "qccsd_from_lag", [energy_eq, t1_equations, l1_equations, t2_equations, l2_equations], ["Energy", "t1=0", "l1=0", "t2=0", "l2=0"])
@@ -65,7 +65,7 @@ def run(dr):
     eval_seq_l = grutils.optimize_equations(dr, [l1_equations, l2_equations], **opti_strat)
     grutils.einsum_raw(dr, "qccsd_l_optimized_from_lag", eval_seq_l)
     drutils.timer.tock("QCCSD l opti done")
-    S
+    
     drutils.save_html(dr, "qccsd_opti_lagrangian", eval_seq_e)
     drutils.save_html(dr, "qccsd_opti_t", eval_seq_t)
     drutils.save_html(dr, "qccsd_opti_l", eval_seq_l)
