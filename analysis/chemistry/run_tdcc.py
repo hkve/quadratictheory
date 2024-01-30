88754567f918940c3d714d6fb630cd681bbdb9be
hkve <haakon.kvernmoen@gmail.com>
2024-01-30 09:37:34 +0100

    New run script

--- analysis/chemistry/run_tdcc.py
+++ analysis/chemistry/run_tdcc.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from tdccsd_absoprtion_spectra.utils import get_pyscf_geometries
 
-from IPython import embed
 
 def run_cc(molecule_name, basis_name, time, pulse, sampler, CC, integrator, general_args={}, integrator_args={}):
     default = {
@@ -46,7 +45,7 @@ def delta_kick(molecule_name, basis_name):
     u = np.array([1.,0.,0.])
     F_str = 1e-3
     dt = 0.025
-    t_end = 0.5
+    t_end = 100
     integrator = "Rk4Integrator"
     integrator_args = {"dt": dt}
 
@@ -68,7 +67,7 @@ def sin2_pulse(molecule_name, basis_name):
     dt = 0.1
     omega = 0.2
     tprime = 2*np.pi/omega
-    t_end = 0.5
+    t_end = 100
     integrator = "Rk4Integrator"
     integrator_args = {"dt": dt}
 
@@ -84,14 +83,59 @@ def sin2_pulse(molecule_name, basis_name):
     run_cc(molecule_name, basis_name, time, pulse, sampler, cf.CCSD, integrator, integrator_args=integrator_args)
     run_cc(molecule_name, basis_name, time, pulse, sampler, cf.QCCSD, integrator, integrator_args=integrator_args)
 
+
+def load_systems(molecule_name, basis_name, pulse, methods, integrators):
+    import os
+    files = os.listdir("dat/")
+
+    results = {method: {} for method in methods}
+
+    for file in files:
+        if not f"_{molecule_name}_" in file: continue 
+        if not f"_{basis_name}_" in file: continue
+        if not f"{pulse}" in file: continue
+
+        for method in methods:
+            for integrator in integrators:
+                if method in file and integrator in file:
+                    results[method][integrator] = np.load(f"dat/{file}", allow_pickle=True)
+    return results
+
+def plot_compare(molecule_name, basis_name, pulse):
+    # integrators = ["GaussIntegrator", "Rk4Integrator"]
+    integrators = ["GaussIntegrator"]
+    methods = ["CCSD", "QCCSD"]
+
+    results = load_systems(molecule_name, basis_name, pulse, methods, integrators)
+
+    fig, ax = plt.subplots(nrows=2, ncols=1, height_ratios=[5,3], figsize=(10,8))
+    fig.suptitle(f"Energy for {molecule_name}({basis_name}) {pulse} pulse ", fontsize=16)
+    for integrator in integrators:
+        t_cc, e_cc = results["CCSD"][integrator]["t"], results["CCSD"][integrator]["energy"] 
+        t_qcc, e_qcc = results["QCCSD"][integrator]["t"], results["QCCSD"][integrator]["energy"] 
+
+        ax[0].plot(t_cc, e_cc, label=f"CCSD: {integrator}")
+        ax[0].plot(t_qcc, e_qcc, label=f"QCCSD: {integrator}")
+
+        ax[1].plot(t_cc, np.abs(e_qcc - e_cc), label=f"DIFF: {integrator}")
+    
+    ax[0].legend()
+    ax[1].legend()
+    ax[1].set_yscale("log")
+
+    plt.show()
+
+
 def main():
     molecule_name, basis_name = "be", "cc-pVDZ"
-    delta_kick(molecule_name, basis_name)
-    sin2_pulse(molecule_name, basis_name)
+    # delta_kick(molecule_name, basis_name)
+    # sin2_pulse(molecule_name, basis_name)
 
-    molecule_name, basis_name = "lih", "cc-pVDZ"
-    delta_kick(molecule_name, basis_name)
-    sin2_pulse(molecule_name, basis_name)
+    # molecule_name, basis_name = "lih", "cc-pVDZ"
+    # delta_kick(molecule_name, basis_name)
+    # sin2_pulse(molecule_name, basis_name)
+
+    plot_compare(molecule_name, basis_name, "DeltaKick")
 
 if __name__ == "__main__":
     main()
