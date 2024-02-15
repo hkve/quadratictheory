52f3a3e97872cab2d6978a6dd514a3af6dbd53ab
hkve <haakon.kvernmoen@gmail.com>
2024-02-15 14:25:58 +0100

    Added density blocks to restricted quadratic scripts

--- symbolic/rqccd.py
+++ symbolic/rqccd.py
@@ -5,6 +5,7 @@ import pathlib as pl
 from sympy import Rational
 from drudge import Stopwatch
 
+
 def load(dr, basename):
     equations = {}
     names = ["t2", "l2", "energy_td_part"]
@@ -16,8 +17,9 @@ def load(dr, basename):
 
     return equations
 
+
 def run(dr, basename):
-    ham = dr.ham  
+    ham = dr.ham
     equations = {}
 
     T2, L2 = drutils.get_restricted_clusters_2(dr)
@@ -25,13 +27,13 @@ def run(dr, basename):
     T = (T2).simplify()
     L = (L2).simplify()
 
-    L_squared = (L*L / 2).simplify()
+    L_squared = (L * L / 2).simplify()
 
     _, X2 = drutils.get_restricted_X(dr)
     _, Y2 = drutils.get_restricted_Y(dr)
 
     ham_bar = drutils.similarity_transform(ham, T)
-    energy_td_part = (L_squared*ham_bar).eval_fermi_vev().simplify()
+    energy_td_part = (L_squared * ham_bar).eval_fermi_vev().simplify()
     energy_td_part = drutils.define_rk0_rhs(dr, energy_td_part)
     equations["energy_td_part"] = energy_td_part
     drutils.save_html(dr, basename + f"_energy_td_part", energy_td_part)
@@ -39,7 +41,7 @@ def run(dr, basename):
     drutils.save_to_pickle(energy_td_part, basename + "_energy_td_part")
     drutils.timer.tock("Done RQCCD energy")
 
-    t2_eq = (Y2*L*ham_bar).eval_fermi_vev().simplify()
+    t2_eq = (Y2 * L * ham_bar).eval_fermi_vev().simplify()
     t2_eq = drutils.define_rk2_rhs_restrictd(dr, t2_eq)
     equations["t2"] = t2_eq
     drutils.save_html(dr, basename + f"_t2", t2_eq)
@@ -49,7 +51,7 @@ def run(dr, basename):
 
     com = ham | X2
     com_bar = drutils.similarity_transform(com, T)
-    l2_eq = (L_squared*com_bar).eval_fermi_vev().simplify()
+    l2_eq = (L_squared * com_bar).eval_fermi_vev().simplify()
     l2_eq = drutils.define_rk2_rhs_restrictd(dr, l2_eq)
     equations["l2"] = l2_eq
     drutils.save_html(dr, basename + f"_l2", l2_eq)
@@ -58,6 +60,7 @@ def run(dr, basename):
     drutils.timer.tock("Done RQCCD l2")
 
     return equations
+
 
 def optimize_expressions(dr, equations, basename):
     for name, eq in equations.items():
@@ -68,10 +71,35 @@ def optimize_expressions(dr, equations, basename):
         drutils.save_html(dr, basename + "_opti_" + name, eval_seq)
         grutils.einsum_raw(dr, basename + "_opti_" + name, eval_seq)
 
+
+def _run_blocks(dr, blocks, block_names):
+    # Get clusters
+    T2, L2 = drutils.get_restricted_clusters_2(dr)
+    rho = [None] * len(blocks)
+
+    L_squared = (L2 * L2 / 2).simplify()
+    # Loop over each block, sim trans and resolve 1 and L term
+    for i, block in enumerate(blocks):
+        block_sim = drutils.similarity_transform(block, T2)
+
+        rho[i] = (L_squared * block_sim).eval_fermi_vev().simplify()
+        drutils.timer.tock(f"Density {block_names[i]}", rho[i])
+
+    return rho
+
+
+def L_densities(dr, basename):
+    # One body
+    o_dums, v_dums = drutils.get_indicies(dr, num=2)
+    blocks, block_names = drutils.get_ob_density_blocks_restricted(dr, o_dums, v_dums)
+    rho = _run_blocks(dr, blocks, block_names)
+    drutils.save_html(dr, basename + "_rho_ob", rho, block_names)
+
+
 if __name__ == "__main__":
     drutils.timer.vocal = True
     dr = drutils.get_restricted_particle_hole_drudge()
-    
+
     basename = "rqccd"
 
     equations = run(dr, basename)
--- symbolic/rqccsd.py
+++ symbolic/rqccsd.py
@@ -5,6 +5,7 @@ import pathlib as pl
 from sympy import Rational
 from drudge import Stopwatch
 
+
 def load(dr, basename):
     equations = {}
     names = ["t2", "l2", "energy_td_part"]
@@ -16,23 +17,24 @@ def load(dr, basename):
 
     return equations
 
+
 def run(dr, basename):
-    ham = dr.ham  
+    ham = dr.ham
     equations = {}
 
     T1, L1 = drutils.get_restricted_clusters_1(dr)
     T2, L2 = drutils.get_restricted_clusters_2(dr)
 
-    T = (T1+T2).simplify()
-    L = (L1+L2).simplify()
+    T = (T1 + T2).simplify()
+    L = (L1 + L2).simplify()
 
-    L_squared = (L*L / 2).simplify()
+    L_squared = (L * L / 2).simplify()
 
     X1, X2 = drutils.get_restricted_X(dr)
     Y1, Y2 = drutils.get_restricted_Y(dr)
 
     ham_bar = drutils.similarity_transform(ham, T)
-    energy_td_part = (L_squared*ham_bar).eval_fermi_vev().simplify()
+    energy_td_part = (L_squared * ham_bar).eval_fermi_vev().simplify()
     energy_td_part = drutils.define_rk0_rhs(dr, energy_td_part)
     equations["energy_td_part"] = energy_td_part
     drutils.save_html(dr, basename + f"_energy_td_part", energy_td_part)
@@ -40,7 +42,7 @@ def run(dr, basename):
     drutils.save_to_pickle(energy_td_part, basename + "_energy_td_part")
     drutils.timer.tock("Done RQCCSD energy")
 
-    t1_eq = (Y1*L*ham_bar).eval_fermi_vev().simplify()
+    t1_eq = (Y1 * L * ham_bar).eval_fermi_vev().simplify()
     t1_eq = drutils.define_rk1_rhs(dr, t1_eq)
     equations["t1"] = t1_eq
     drutils.save_html(dr, basename + f"_t1", t1_eq)
@@ -48,7 +50,7 @@ def run(dr, basename):
     drutils.save_to_pickle(t1_eq, basename + "_t1")
     drutils.timer.tock("Done RQCCSD t1")
 
-    t2_eq = (Y2*L*ham_bar).eval_fermi_vev().simplify()
+    t2_eq = (Y2 * L * ham_bar).eval_fermi_vev().simplify()
     t2_eq = drutils.define_rk2_rhs_restrictd(dr, t2_eq)
     equations["t2"] = t2_eq
     drutils.save_html(dr, basename + f"_t2", t2_eq)
@@ -58,7 +60,7 @@ def run(dr, basename):
 
     com = ham | X1
     com_bar = drutils.similarity_transform(com, T)
-    l1_eq = (L_squared*com_bar).eval_fermi_vev().simplify()
+    l1_eq = (L_squared * com_bar).eval_fermi_vev().simplify()
     l1_eq = drutils.define_rk1_rhs(dr, l1_eq)
     equations["l1"] = l1_eq
     drutils.save_html(dr, basename + f"_l1", l1_eq)
@@ -68,7 +70,7 @@ def run(dr, basename):
 
     com = ham | X2
     com_bar = drutils.similarity_transform(com, T)
-    l2_eq = (L_squared*com_bar).eval_fermi_vev().simplify()
+    l2_eq = (L_squared * com_bar).eval_fermi_vev().simplify()
     l2_eq = drutils.define_rk2_rhs_restrictd(dr, l2_eq)
     equations["l2"] = l2_eq
     drutils.save_html(dr, basename + f"_l2", l2_eq)
@@ -77,6 +79,7 @@ def run(dr, basename):
     drutils.timer.tock("Done RQCCSD l2")
 
     return equations
+
 
 def optimize_expressions(dr, equations, basename):
     for name, eq in equations.items():
@@ -87,10 +90,40 @@ def optimize_expressions(dr, equations, basename):
         drutils.save_html(dr, basename + "_opti_" + name, eval_seq)
         grutils.einsum_raw(dr, basename + "_opti_" + name, eval_seq)
 
+
+def _run_blocks(dr, blocks, block_names):
+    # Get clusters
+    T1, L1 = drutils.get_restricted_clusters_1(dr)
+    T2, L2 = drutils.get_restricted_clusters_2(dr)
+    rho = [None] * len(blocks)
+
+    T = (T1 + T2).simplify()
+    L = (L1 + L2).simplify()
+
+    L_squared = (L * L / 2).simplify()
+
+    # Loop over each block, sim trans and resolve 1 and L term
+    for i, block in enumerate(blocks):
+        block_sim = drutils.similarity_transform(block, T)
+
+        rho[i] = (L_squared * block_sim).simplify()
+        drutils.timer.tock(f"Density {block_names[i]}", rho[i])
+
+    return rho
+
+
+def L_densities(dr, basename):
+    # One body
+    o_dums, v_dums = drutils.get_indicies(dr, num=2)
+    blocks, block_names = drutils.get_ob_density_blocks_restricted(dr, o_dums, v_dums)
+    rho = _run_blocks(dr, blocks, block_names)
+    drutils.save_html(dr, basename + "_rho_ob", rho, block_names)
+
+
 if __name__ == "__main__":
     drutils.timer.vocal = True
     dr = drutils.get_restricted_particle_hole_drudge()
-    
+
     basename = "rqccd"
 
     equations = run(dr, basename)
