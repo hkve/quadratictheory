893f52d1c9266968201e27f855d294de1ddb0d36
hkve <haakon.kvernmoen@gmail.com>
2024-02-18 18:30:15 +0100

    More permutation stupidness

--- symbolic/permutations.py
+++ symbolic/permutations.py
@@ -7,38 +7,48 @@ from sympy.physics.secondquant import PermutationOperator, simplify_index_permut
 
 # For Restricted Permutation Operator
 from sympy import Expr, sympify, default_sort_key, Basic, Dummy, S
-# from sympy.core.expr import Expr
-# from sympy.core.sympify import sympify
-# from sympy.core.sorting import default_sort_key
-# from sympy.core.basic import Basic
-# from sympy.core.symbol import Dummy
-# from sympy.core.singleton import S
+from sympy import Mul
 
 from IPython import embed
 
 def permutations(self, P_list=None):
+    restricted = self.drudge.__class__.__name__ == "RestrictedPartHoleDrudge"
+
     if P_list is None:
-        (i,j), (a,b) = drutils.get_indicies(self.drudge, num=2)
-        P_ij = PermutationOperator(i,j)
-        P_ab = PermutationOperator(a,b)
-        P_list = [P_ij, P_ab]
+        if restricted:
+            P_list = _default_restricted_permutations(self.drudge)
+        else:
+            P_list = _default_general_permutations(self.drudge)
 
     if type(P_list) != list:
         P_list = [P_list]
 
-    sympy_amps = _simplify_index_permutations(self, self._terms, expanded=self._expanded, P_list=P_list)
+    sympy_amps = _simplify_index_permutations(self, self._terms, expanded=self._expanded, P_list=P_list, restricted=restricted)
 
     return self.drudge.einst(sympy_amps)
+
+def _default_general_permutations(dr):
+    (i,j), (a,b) = drutils.get_indicies(dr, num=2)
+    P_ij = PermutationOperator(i,j)
+    P_ab = PermutationOperator(a,b)
+
+    return [P_ij, P_ab]
+
+def _default_restricted_permutations(dr):
+    (i,j), (a,b) = drutils.get_indicies(dr, num=2)
+    P = RestrictedPermutationOperator(i,j,a,b)
+
+    return [P]
 
 def _is_in_alphabetical_order(indicies):
     str_indicies = [str(i) for i in indicies]
     return str_indicies == sorted(str_indicies)
 
-def _sort_alphabetically(amps_sympy):
+def _sort_alphabetically(amps_sympy, restricted):
     new_amps_sympy = 0
     for term in amps_sympy.args:
         new_amplitudes = []
-        
+
         term_new = 1
         for amplitude in term.as_ordered_factors():
             if type(amplitude) == Indexed:
@@ -48,18 +58,10 @@ def _sort_alphabetically(amps_sympy):
                     term_new *= amplitude
                     continue
                 
-                p, q, r, s = indicies
-
-                permute_bra = not _is_in_alphabetical_order([p, q])
-                permute_ket = not _is_in_alphabetical_order([r, s])
-                
-                amplitude_new = amplitude.copy()
-                if permute_bra:
-                    P = PermutationOperator(p, q)
-                    amplitude_new = P.get_permuted(amplitude_new)
-                if permute_ket:
-                    P = PermutationOperator(r, s)
-                    amplitude_new = P.get_permuted(amplitude_new)
+                if restricted:
+                    amplitude_new = _get_restricted_alphabetical_ordered(amplitude, indicies)
+                else:
+                    amplitude_new = _get_alphabetical_ordered(amplitude, indicies)
 
                 term_new *= amplitude_new
             
@@ -70,20 +72,88 @@ def _sort_alphabetically(amps_sympy):
                 
     return new_amps_sympy
 
-def _simplify_index_permutations(self, terms, expanded, P_list):
+
+def _get_alphabetical_ordered(amplitude, indicies):
+    p, q, r, s = indicies
+
+    permute_bra = not _is_in_alphabetical_order([p, q])
+    permute_ket = not _is_in_alphabetical_order([r, s])
+    
+    amplitude_new = amplitude.copy()
+    if permute_bra:
+        P = PermutationOperator(p, q)
+        amplitude_new = P.get_permuted(amplitude_new)
+    if permute_ket:
+        P = PermutationOperator(r, s)
+        amplitude_new = P.get_permuted(amplitude_new)
+
+    return amplitude_new
+
+def _get_restricted_alphabetical_ordered(amplitude, indicies):
+    p, q, r, s = indicies
+    permute_bra = not _is_in_alphabetical_order([p, q])
+    permute_ket = not _is_in_alphabetical_order([r, s])
+
+    both_free_bra = all([str(i) in ["a", "b", "i", "j"] for i in [p,q]])
+    both_free_ket = all([str(i) in ["a", "b", "i", "j"] for i in [r,s]])
+
+    amplitude_new = amplitude.copy()
+    P = RestrictedPermutationOperator(p, q, r, s)
+    if permute_bra and not both_free_bra:
+        amplitude_new = P.get_permuted(amplitude_new)
+    elif permute_ket and not both_free_ket:
+        amplitude_new = P.get_permuted(amplitude_new)
+    
+    return amplitude_new
+
+def _simplify_index_permutations(self, terms, expanded, P_list, restricted):
     if not expanded:
         terms = self._expand(terms)
 
     resolvers = self._drudge.resolvers
 
-    amps = terms.map(lambda x: x.amp)
+    amps_sympy = 0
+    for term in terms.collect():
+        fixed_term = _fix_strange_u(self._drudge, term)
+        amps_sympy += fixed_term.amp
 
-    amps_sympy = sum(list(amps.collect()))
-    amps_sympy = _sort_alphabetically(amps_sympy)
+    # amps = terms.map(lambda x: x.amp)
+    # amps_sympy = sum(list(amps.collect()))
+        
+    amps_sympy = _sort_alphabetically(amps_sympy, restricted)
 
     amps_sympy = simplify_index_permutations(amps_sympy, P_list)
 
     return amps_sympy
+
+def _swap_dummy_index(dr, term, p, q):
+    pdums, hdums, = dr.DEFAULT_PART_DUMMS, dr.DEFAULT_HOLE_DUMMS
+
+    is_part = p in pdums and q in pdums
+    is_hole = p in hdums and q in hdums
+
+    assert is_part != is_hole, f"Must swap dummy indicies with same range {p}, {q} are from different ranges"
+
+    dums = pdums if is_part else hdums
+
+    term = term.subst({p: dums[-1]})
+    term = term.subst({q: p})
+    term = term.subst({dums[-1]: q})
+
+    return term
+
+def _fix_strange_u(dr, term):
+    (i,j,k,l), (a,b,c,d) = drutils.get_indicies(dr, num=4)
+    u = IndexedBase("u")
+
+    for part in term.exprs:
+        if type(part) == Mul:
+            if part.has(u[k,l,d,c]):
+                term = _swap_dummy_index(dr, term, c, d) 
+            if part.has(u[l,k,c,d]):
+                term = _swap_dummy_index(dr, term, k, l)
+
+    return term 
 
 class RestrictedPermutationOperator(Expr):
     """
