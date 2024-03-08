2f286d925a1696bfe2a6ce4c987a6b3e22f9e08d
hkve <haakon.kvernmoen@gmail.com>
2024-03-08 16:45:50 +0100

    Symbolic W_0, W_S, W_D, W_T, W_Q

--- symbolic/quadratic_weights.py
+++ symbolic/quadratic_weights.py
@@ -1,6 +1,7 @@
 import drudge_utils as drutils
 import gristmill_utils as grutils
 from sympy import Rational, Symbol, IndexedBase
+from permutations import permutations
 from IPython import embed
 
 def save(dr, name, equation):
@@ -100,30 +101,16 @@ def double_excited(dr):
 
     (i, j), (a, b) = drutils.get_indicies(dr, num=2)
     X2 = drutils.get_X(dr, 2, (i,j), (a,b))
-    
-    # Double excitations
-    expr = L1*L1
-    res = (expr*X2).eval_fermi_vev().simplify()
-    drutils.timer.tock(f"Doubles done", res)
 
-    # Triple excitations
-    expr = -2*L1*L2*T1
-    res += (expr*X2).eval_fermi_vev().simplify()
-    drutils.timer.tock(f"Triples done", res)
-    
-    # Quadruple excitations
-    expr = -L2*L2*T2
-    res += (expr*X2).eval_fermi_vev().simplify()
-    
-    expr = Rational(1,2)*L2*L2*T1*T1
-    res += (expr*X2).eval_fermi_vev().simplify()
-    drutils.timer.tock(f"Quadruples done", res)
+    expr = L1*L1 - 2*L1*L2*T1 + L2*L2*(-T2 + Rational(1,2)*T1*T1)
 
     # Add overall factor and calculate
-    res = (Rational(1,2)*res).simplify()
+    res = (Rational(1,2)*expr*X2).eval_fermi_vev().simplify()
 
-    res = dr.define(IndexedBase(f"det")[a,b,i,j], res)
-    save(dr, "qccsd_2p2h_weight", res)
+    res = dr.define(IndexedBase(f"bra")[a,b,i,j], res)
+    # save(dr, "qccsd_2p2h_weight", res)
+    from IPython import embed
+    embed()
 
 def triple_excited(dr):
     T1, T2, L1, L2 = get_operators(dr)
@@ -132,25 +119,67 @@ def triple_excited(dr):
     X3 = drutils.get_X(dr, 3, (i,j,k), (a,b,c))
     Y3 = drutils.get_Y(dr, 3, (i,j,k), (a,b,c))
    
-    expr = T1*T2
+    expr = T1*T2 + Rational(1,6)*T1*T1*T1
     ket_res = (Y3*expr).eval_fermi_vev().simplify()
 
-    expr = Rational(1,6)*T1*T1*T1
-    ket_res += (Y3*expr).eval_fermi_vev().simplify()
-
-    expr = 2*L1*L2
+    expr = 2*L1*L2 -L2*L2*T1
     bra_res = (expr*X3).eval_fermi_vev().simplify()
 
-    expr = -L2*L2*T1
-    bra_res += (expr*X3).eval_fermi_vev().simplify()
-
-    bra_res *= Rational(1,2)
-
     bra_res = bra_res.simplify()
+    bra_res *= Rational(1,2)
+    
     ket_res = ket_res.simplify()
 
-    from IPython import embed
-    embed()
+    bra_lhs = IndexedBase("bra")
+    dr.set_dbbar_base(bra_lhs, 3)
+    bra = dr.define(bra_lhs[a,b,c,i,j,k], bra_res)
+
+    ket_lhs = IndexedBase("ket")
+    dr.set_dbbar_base(ket_lhs, 3)
+    ket = dr.define(ket_lhs[a,b,c,i,j,k], ket_res)
+
+    res = dr.einst(Rational(1,36)*bra*ket).simplify()
+
+    W_T_lhs = Symbol("WT")
+    W_T = dr.define(W_T_lhs, res)
+
+    grutils.einsum_raw(dr, "triples_weight", W_T)
+
+
+def quadrouple_excited(dr):
+    T1, T2, L1, L2 = get_operators(dr)
+
+    (i, j, k, l), (a, b, c, d) = drutils.get_indicies(dr, num=4)
+    X4 = drutils.get_X(dr, 4, (i,j,k,l), (a,b,c,d))
+    Y4 = drutils.get_Y(dr, 4, (i,j,k,l), (a,b,c,d))
+   
+    expr = Rational(1,2)*T2*T2 + Rational(1,4)*T1*T1*T2
+    ket_res = (Y4*expr).eval_fermi_vev().simplify()
+
+    expr = L2*L2
+    bra_res = (expr*X4).eval_fermi_vev().simplify()
+
+    bra_res = bra_res.simplify()
+    bra_res *= Rational(1,2)
+    
+    ket_res = ket_res.simplify()
+
+    bra_lhs = IndexedBase("bra")
+    dr.set_dbbar_base(bra_lhs, 4)
+    bra = dr.define(bra_lhs[a,b,c,d,i,j,k,l], bra_res)
+
+    ket_lhs = IndexedBase("ket")
+    dr.set_dbbar_base(ket_lhs, 4)
+    ket = dr.define(ket_lhs[a,b,c,d,i,j,k,l], ket_res)
+
+    res = dr.einst(Rational(1,576)*bra*ket).simplify()
+
+    W_Q_lhs = Symbol("WQ")
+    W_Q = dr.define(W_Q_lhs, res)
+
+    grutils.einsum_raw(dr, "quadruplets_weight", W_Q)
+
+
 
 if __name__ == "__main__":
     dr = drutils.get_particle_hole_drudge(dummy=True)
@@ -158,5 +187,6 @@ if __name__ == "__main__":
     drutils.timer.vocal = True
     # reference(dr)
     # single_excited(dr)
-    # double_excited(dr)
-    triple_excited(dr)
+    double_excited(dr)
+    # triple_excited(dr)
+    # quadrouple_excited(dr)
