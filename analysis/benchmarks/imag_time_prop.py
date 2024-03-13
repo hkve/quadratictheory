12249e50e95b2766021594b93181d976b7316d09
hkve <haakon.kvernmoen@gmail.com>
2024-03-13 17:33:17 +0100

    Tested some plotting, think 2x axis is the way to go

--- analysis/benchmarks/imag_time_prop.py
+++ analysis/benchmarks/imag_time_prop.py
@@ -1,6 +1,7 @@
 import clusterfock as cf
 import numpy as np
 import matplotlib.pyplot as plt
+import plot_utils as pl
 
 def run_imag_timeprop(name, geometry, basis, CC, **kwargs):
     default = {
@@ -47,29 +48,91 @@ def run_imag_timeprop(name, geometry, basis, CC, **kwargs):
     filename = f"ImagTimeProp_{CC.__name__}_{name}_{basis}_{integrator}_{time[0]}_{time[1]}_{time[2]}"
     np.savez(f"{path}/{filename}", **results)
 
-def plot_imag_timeprop(filename):
+def plot_imag_timeprop(filename, **kwargs):
     defaults = {
         "path": "dat",
+        "tol": None,
     }
 
+    defautls = defaults.update(**kwargs)
+
+    tol = defaults["tol"]
     path = defaults["path"]
 
     results = np.load(f"{path}/{filename}", allow_pickle=True)
     
+    dE_i = np.abs(results["energy"][1:] - results["energy"][0:-1])
+
     dE = np.abs(results["gs_energy"] - results["energy"])
     time = results["t"]
 
-    print(dE)
-
     fig, ax = plt.subplots()
     ax.plot(time, dE, label=r"$\Delta E$")
-    ax.plot(time, results["delta_t1"], label=r"$|\Delta t_1|$")
+    ax.plot(time[1:], dE_i, label=r"$|E_{i+1} - E_i|$")
     ax.plot(time, results["delta_t2"], label=r"$|\Delta t_2|$")
-    ax.plot(time, results["delta_l1"], label=r"$|\Delta l_1|$")
     ax.plot(time, results["delta_l2"], label=r"$|\Delta l_2|$")
+
+    keys = list(results.keys())
+    if "delta_t1" in keys:
+        ax.plot(time, results["delta_t1"], label=r"$|\Delta t_1|$")
+    if "delta_l1" in keys:
+        ax.plot(time, results["delta_l1"], label=r"$|\Delta l_1|$")
+
+    if tol:
+        ax.hlines(tol, time.min(), time.max(), color="gray", alpha=0.5, ls="--")
+    
     ax.set(xlabel=r"$\pi_{\pm}$")
     ax.legend()
     ax.set_yscale("log")
+    plt.show()
+
+def plot_two_imag_timeprop(filenames, **kwargs):
+    defaults = {
+        "path": "dat",
+        "tol": None,
+    }
+    defautls = defaults.update(**kwargs)
+
+    tol = defaults["tol"]
+    path = defaults["path"]
+    ls = ["-", "--", "-."]
+
+    fig, ax = plt.subplots()
+    for i, filename in enumerate(filenames):
+        results = np.load(f"{path}/{filename}", allow_pickle=True)
+
+        dE_i = np.abs(results["energy"][1:] - results["energy"][0:-1])
+        dE = np.abs(results["gs_energy"] - results["energy"])
+        time = results["t"]
+
+        ax.plot(time, dE, label=r"$\Delta E$", color=pl.colors[i], ls=ls[0])
+        ax.plot(time[1:], dE_i, label=r"$|E_{i+1} - E_i|$", color=pl.colors[i], ls=ls[1])
+
+    ax.set(xlabel=r"$\pi_{\pm} = \pm it$ [au]", ylabel="Energy [au]")
+    ax.legend()
+    ax.set_yscale("log")
+    plt.show()
+
+    fig, ax = plt.subplots()
+    for i, filename in enumerate(filenames):
+        results = np.load(f"{path}/{filename}", allow_pickle=True)
+        time = results["t"]
+
+        ax.plot(time, results["delta_t2"], label=r"$|\Delta t_2|$", ls=ls[i])
+        ax.plot(time, results["delta_l2"], label=r"$|\Delta l_2|$", ls=ls[i])
+
+        keys = list(results.keys())
+        if "delta_t1" in keys:
+            ax.plot(time, results["delta_t1"], label=r"$|\Delta t_1|$", ls=ls[i])
+        if "delta_l1" in keys:
+            ax.plot(time, results["delta_l1"], label=r"$|\Delta l_1|$", ls=ls[i])
+
+        if tol:
+            ax.hlines(tol, time.min(), time.max(), color="gray", alpha=0.5, ls="--")
+        
+        ax.set(xlabel=r"$\pi_{\pm}$")
+        ax.legend()
+        ax.set_yscale("log")
     plt.show()
 
 def run():
@@ -99,8 +162,13 @@ def run():
 
 
 def plot():
-    plot_imag_timeprop("ImagTimeProp_CCSD_lih_cc-pVDZ_Rk4Integrator_0_100_0.05.npz")
+    plot_imag_timeprop("ImagTimeProp_CCSD_lih_cc-pVDZ_Rk4Integrator_0_200_0.05.npz", tol=1e-10)
 
+    # filenames = [
+    #     "ImagTimeProp_CCD_lih_cc-pVDZ_Rk4Integrator_0_100_0.05.npz",
+    #     "ImagTimeProp_QCCD_lih_cc-pVDZ_Rk4Integrator_0_100_0.05.npz"
+    # ]
+    # plot_two_imag_timeprop(filenames, tol=1e-10)
 if __name__ == "__main__":
     # run()
     plot()
