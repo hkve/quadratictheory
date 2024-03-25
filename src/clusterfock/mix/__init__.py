e8f5e05bcd07718f8200acb907b585b3a3d45aee
hkve <haakon.kvernmoen@gmail.com>
2024-03-25 10:51:37 +0100

    Added combined RelaxedMixer and DIISMixer, named SoftStartDIISMixer. Runs RelaxedMixer up to a number of iterations then apply DIIS

--- src/clusterfock/mix/__init__.py
+++ src/clusterfock/mix/__init__.py
@@ -1,3 +1,4 @@
 from clusterfock.mix.mix import Mixer
 from clusterfock.mix.relaxedmixer import RelaxedMixer
 from clusterfock.mix.DIIS import DIISMixer
+from clusterfock.mix.softstart_DIIS import SoftStartDIISMixer
