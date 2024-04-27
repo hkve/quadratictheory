bf52d4b9d9a922994bf57c6de69722b2d9b5f805
hkve <haakon.kvernmoen@gmail.com>
2024-04-27 13:50:48 +0200

    H4 rectangle plot fixing

--- analysis/chemistry/h4_rectangle.py
+++ analysis/chemistry/h4_rectangle.py
@@ -7,6 +7,7 @@ from pyscf.ci.cisd import tn_addrs_signs
 from pyscf import lib
 lib.num_threads(1)
 
+import plotting.plot_utils as pl
 
 def get_geometry(theta):
     theta1 = (theta / 2) * np.pi / 180
@@ -97,15 +98,15 @@ def run_fci(geometry, basis):
 
 
 def run_ccsd(geometry, basis, quad=False):
-    b = cf.PyscfBasis(geometry, basis, restricted=False).pyscf_hartree_fock()
+    b = cf.PyscfBasis(geometry, basis).pyscf_hartree_fock()
     b.from_restricted()
 
     cc = cf.QCCSD(b) if quad else cf.CCSD(b)
 
     if quad:
-        cc.run()
+        cc.run(tol=1e-4)
     else:
-        cc.run(include_l=True)
+        cc.run(include_l=True, tol=1e-6)
 
     e_cc = cc.energy()
 
@@ -121,11 +122,9 @@ def run_ccsd(geometry, basis, quad=False):
     return e_cc, W0, WS, WD, WT, WQ
 
 def run():
-    thetas = np.arange(80,100+1,1)
-    basis = "DZ"
+    thetas = np.arange(80,100+1,1).astype(float)
+    basis = "dzp"
 
-    print(thetas)
-    exit()
     filename = f"dat/H4_rectangle_{basis}_"
     filename_fci = filename + "fci.npz"
     filename_ccsd = filename + "ccsd.npz"
@@ -137,7 +136,8 @@ def run():
         "WS": np.zeros_like(thetas),
         "WD": np.zeros_like(thetas),
     }
-    for i, theta in next(thetas):
+    
+    for i, theta in enumerate(thetas):
         geometry = get_geometry(theta) 
         e_fci, W0, WS_max, WD_max = run_fci(geometry, basis)
         fci["energy"][i] = e_fci
@@ -145,7 +145,8 @@ def run():
         fci["WS"][i] = WS_max
         fci["WD"][i] = WD_max
         print(f"FCI {theta = } done")
-    np.saves(filename_fci, **fci)
+    
+    np.savez(filename_fci, **fci)
 
     ccsd = {
         "energy": np.zeros_like(thetas),
@@ -153,7 +154,7 @@ def run():
         "WS": np.zeros_like(thetas),
         "WD": np.zeros_like(thetas),
     }
-    for i, theta in next(thetas):
+    for i, theta in enumerate(thetas):
         geometry = get_geometry(theta) 
         e_ccsd, W0, WS_max, WD_max, _, _ = run_ccsd(geometry, basis, quad=False)
         ccsd["energy"][i] = e_ccsd
@@ -162,7 +163,7 @@ def run():
         ccsd["WD"][i] = WD_max
         print(f"CCSD {theta = } done")
 
-    np.saves(filename_ccsd, **ccsd)
+    np.savez(filename_ccsd, **ccsd)
 
     qccsd = {
         "energy": np.zeros_like(thetas),
@@ -172,7 +173,7 @@ def run():
         "WT": np.zeros_like(thetas),
         "WQ": np.zeros_like(thetas),
     }
-    for i, theta in next(thetas):
+    for i, theta in enumerate(thetas):
         geometry = get_geometry(theta) 
         e_qccsd, W0, WS_max, WD_max, WT, WQ = run_ccsd(geometry, basis, quad=True)
         qccsd["energy"][i] = e_qccsd
@@ -183,7 +184,74 @@ def run():
         qccsd["WQ"][i] = WQ
         print(f"QCCSD {theta = } done")
 
-    np.saves(filename_qccsd, **qccsd)
+    np.savez(filename_qccsd, **qccsd)
+
+def get_data(basis):
+    filename = f"dat/H4_rectangle_{basis}_"
+    filename_fci = filename + "fci.npz"
+    filename_ccsd = filename + "ccsd.npz"
+    filename_qccsd = filename + "qccsd.npz"
+
+    fci = np.load(filename_fci, allow_pickle=True)
+    ccsd = np.load(filename_ccsd, allow_pickle=True)
+    qccsd = np.load(filename_qccsd, allow_pickle=True)
+
+    return fci, ccsd, qccsd
+
+def plot_energy():
+    fci, ccsd, qccsd = get_data("dzp")
+    thetas = np.arange(80,101,1)
+    c = pl.colors
+
+    fig, ax = plt.subplots(nrows=2, ncols=1, sharex=True, figsize=(6,8), height_ratios=[6,3])
+
+    ax[0].plot(thetas, fci["energy"], ls="-", marker=".", label="FCI", c=c[0])
+    ax[0].plot(thetas, ccsd["energy"], ls="-", marker=".", label="CCSD", c=c[1])
+    ax[0].plot(thetas, qccsd["energy"], ls="-", marker=".", label="QCCSD", c=c[2])
+    ax[0].legend()
+    ax[0].set(ylabel=r"Energy [au]")
+    
+    ax[1].plot(thetas, np.abs(ccsd["energy"]-fci["energy"]), ls="-", marker=".", label="CCSD", c=c[1])
+    ax[1].plot(thetas, np.abs(qccsd["energy"]-fci["energy"]), ls="-", marker=".", label="QCCSD", c=c[2])
+    ax[1].legend()
+    ax[1].set(xlabel=r"$\theta$", ylabel=r"$|E - E_{FCI}|$ [au]")
+    plt.show()
+
+def plot_weigths():
+    fci, ccsd, qccsd = get_data("dzp")
+
+    fig, ax = plt.subplots(nrows=3, ncols=1, sharex=True, figsize=(6,8))
+    thetas = np.arange(80,101,1)
+
+    fci_line, = ax[0].plot(thetas, fci["W0"], ls="-", marker=".", label="FCI $W_0$")
+    ccsd_line, = ax[0].plot(thetas, ccsd["W0"], ls="-", marker=".", label="CCSD $W_0$")
+    qccsd_line, = ax[0].plot(thetas, qccsd["W0"], ls="-", marker=".", label="QCCSD $W_0$")
+    ax[0].set(ylabel="$W_0, P_0$")
+
+    ax[1].plot(thetas, fci["WS"], ls="-", marker=".", label="FCI max($W_S$)")
+    ax[1].plot(thetas, ccsd["WS"], ls="-", marker=".", label="CCSD max($W_S$)")
+    ax[1].plot(thetas, qccsd["WS"], ls="-", marker=".", label="QCCSD max($W_S$)")
+
+
+    ax[2].plot(thetas, fci["WD"], ls="-", marker=".", label="FCI max($W_D$)")
+    ax[2].plot(thetas, ccsd["WD"], ls="-", marker=".", label="CCSD max($W_D$)")
+    ax[2].plot(thetas, qccsd["WD"], ls="-", marker=".", label="QCCSD max($W_D$)")
+    
+    lines = [fci_line, ccsd_line, qccsd_line]
+    labels = ["FCI", "CCSD", "QCCSD"]
+    plt.figlegend(lines, labels, loc = 'upper center', ncol=5, labelspacing=0.)
+    ax[2].set(xlabel=r"$\theta$")
+    plt.show()
+
+
+    exit()
+    fig, ax = plt.subplots()
+    ax.plot(thetas, qccsd["WT"], label="WT", ls="-", marker=".")
+    ax.plot(thetas, qccsd["WQ"], label="WQ", ls="-", marker=".")
+    ax.legend()
+    plt.show()
 
 if __name__ == "__main__":
-    run()
+    # run()
+    # plot_energy()
+    plot_weigths()
